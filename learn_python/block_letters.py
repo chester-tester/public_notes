6584c17b34df4a8ce9ef39137b185866dd661529
Shawn Blake <45636772+chester-tester@users.noreply.github.com>
2024-05-22 14:06:31 -0600

    Update block_letters.py

--- learn_python/block_letters.py
+++ learn_python/block_letters.py
@@ -1,1 +1,14 @@
+# inserting block lettering as multi-line strings to save time 
 
+name = ''' 
+   sss     ccc    bbbb 
+  s   s   c   c   b   b
+  s       c       b   b
+   sss    c       bbbb 
+      s   c       b   b
+  s   s   c   c   b   b
+   sss     ccc    bbbb     
+  '''
+
+
+print(name)
